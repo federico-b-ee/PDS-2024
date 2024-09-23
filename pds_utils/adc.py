8cffd4afacf0bc4767bbbd36d25f8a3048268a10
Federico Borello <f.borello@outlook.com>
2024-09-23 00:24:14 -0300

    fix TS4

--- pds_utils/adc.py
+++ pds_utils/adc.py
@@ -2,6 +2,7 @@ from typing import Tuple
 
 import numpy as np
 
+
 def adc(
     signal: np.ndarray,
     bits: int = 8,
@@ -21,7 +22,7 @@ def adc(
         np.ndarray: Signal with Quantization process.
         float: Quantization steps
     """
-    levels = 2**(bits-1)
+    levels = 2 ** (bits - 1)
     q = (vcc) / (levels)
 
     s = np.clip(signal, vee, vcc)
@@ -31,4 +32,27 @@ def adc(
     s = np.round(s)
 
     s = s * q + vee
+    return (s, q)
+
+
+def adc_no_clip(
+    signal: np.ndarray,
+    bits: int = 8,
+    vcc: float = 3.3,
+):
+    """Returns a signal simualating the quantization process of an ADC
+
+    Args:
+        signal (np.ndarray): Signal.
+        bits (int): Bits for the Quantization.
+        vcc (float): Positive supply voltage.
+
+
+    Returns:
+        np.ndarray: Signal with Quantization process.
+        float: Quantization steps
+    """
+
+    q = vcc / 2**bits
+    s = np.round(signal / q) * q
     return (s, q)
